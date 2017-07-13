a575cf2137e05a06d2f1ac6bae7021c4f818fb4f
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-07-13 12:34:08 +0200

    Restructuring

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RootComponent from './components/stateful/MainMarket/MainMarket.component.jsx';
+require('./styles/index.css');
 
 ReactDOM.render(<RootComponent/>, document.getElementById('container'));

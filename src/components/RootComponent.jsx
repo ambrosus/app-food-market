f54ac6a5dee8299667f4da8766ef4a59cf29f401
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-07-21 18:39:34 +0200

    add redux & react-redux

--- src/components/RootComponent.jsx
+++ src/components/RootComponent.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import ProductContainer from './stateful/ProductContainer/ProductContainer.jsx';
 import NavigationBar from './stateless/NavigationBar/NavigationBar.jsx';
 import PageContainer from "./stateless/PageContainer/PageContainer.jsx";
+import { Provider } from 'react-redux';
+import marketApp from '../redux/reducers/index.js';
+import { createStore } from 'redux';
 require('./RootComponent.scss');
+
+
 
 export default class RootComponent extends Component {
     render() {
+    	let store = createStore(marketApp);
         return (
-            <PageContainer>
-                <NavigationBar/>
-                <ProductContainer/>
-            </PageContainer>)
+			<Provider store={store}>
+	            <PageContainer>
+	                <NavigationBar/>
+	                <ProductContainer/>
+	            </PageContainer>
+        	</Provider>)
     }
 }
--- src/components/stateless/NavigationBar/NavigationBar.jsx
+++ src/components/stateless/NavigationBar/NavigationBar.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import Button from "../../stateless/Button/Button.jsx";
+import EthereumTransactionsStatus from "../../stateful/EthereumTransactionsStatus/EthereumTransactionsStatus.jsx";
 require('./NavigationBar.scss');
 
 export default class NavigationBar extends Component {
-
     render() {
         return (<div className="navigation">
             <span className="navigation__title">Market</span>
+            <EthereumTransactionsStatus />
             <Button className='navigation__create-offer-button'>
                 <span className="icon-basket-loaded button-icon-default"/><span>Create an offer</span>
             </Button>

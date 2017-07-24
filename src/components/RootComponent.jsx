7b221656dfcc864f712ddf7a447b45af814ce057
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-07-24 15:21:08 +0200

    Create offer page

--- src/components/RootComponent.jsx
+++ src/components/RootComponent.jsx
@@ -3,6 +3,7 @@ import React, {Component} from "react";
 import PageContainer from "./stateless/PageContainer/PageContainer.jsx";
 import MarketPage from "./stateful/Pages/MarketPage/MarketPage.jsx";
 import OrdersPage from "./stateful/Pages/OrdersPage/OrdersPage.jsx";
+import ProfilePage from "./stateful/Pages/ProfilePage/ProfilePage.jsx";
 import CreateOfferPage from "./stateful/Pages/CreateOfferPage/CreateOfferPage.jsx";
 import TopContainer from "./stateful/TopContainer/TopContainer.jsx";
 import { Provider } from "react-redux";
@@ -19,7 +20,8 @@ export default class RootComponent extends Component {
                     <TopContainer/>
                     <Route exact path="/" component={MarketPage}/>
                     <Route exact path="/orders" component={OrdersPage}/>
-                    <Route exact path="/profile" component={CreateOfferPage}/>
+                    <Route exact path="/profile" component={ProfilePage}/>
+                    <Route exact path="/create-offer" component={CreateOfferPage}/>
                 </PageContainer>
             </Router>
         </Provider>)

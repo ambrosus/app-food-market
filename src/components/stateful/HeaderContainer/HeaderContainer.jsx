7f624239f78e57a63b833fe802494ca4449078e0
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-07-25 15:00:56 +0200

    transaction list

--- src/components/stateful/HeaderContainer/HeaderContainer.jsx
+++ src/components/stateful/HeaderContainer/HeaderContainer.jsx
@@ -2,10 +2,12 @@ import React, {Component} from "react";
 import Header from '../../stateless/Header/Header.jsx';
 import ContextMenu from '../../stateless/ContextMenu/ContextMenu.jsx';
 import EthereumTransactionsStatus from '../../stateful/EthereumTransactionsStatus/EthereumTransactionsStatus.jsx';
+import EthereumTransactionList from '../../stateful/EthereumTransactionList/EthereumTransactionList.jsx';
 
 let HeaderContainer = () => (
     <Header>
         <img className="logo" src="/static/images/logotype.png"/>
+		<EthereumTransactionList/>
 		<EthereumTransactionsStatus/>
         <ContextMenu/>
         <hr className="line"/>

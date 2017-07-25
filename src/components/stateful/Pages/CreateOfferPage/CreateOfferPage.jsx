de5298176e8c56dab05c33ad943af40cfeaf0111
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-07-25 12:48:10 +0200

    Create offer styling

--- src/components/stateful/Pages/CreateOfferPage/CreateOfferPage.jsx
+++ src/components/stateful/Pages/CreateOfferPage/CreateOfferPage.jsx
@@ -1,20 +1,32 @@
 import React, {Component} from "react";
+import classNames from "classnames";
 import NavigationBar from "../../../stateless/NavigationBar/NavigationBar.jsx";
 import {Link} from "react-router-dom";
 import Button from "../../../stateless/Button/Button.jsx";
 import TextField from "../../../stateless/TextField/TextField.jsx";
-require("./CreateOfferPage.scss");
+import styles from "./CreateOfferPage.scss";
 
 class CreateOfferPage extends Component {
     render() {
         return (<div>
             <NavigationBar title="Create new offer">
-                <Button className="navigation__cancel-button">Cancel</Button>
-                <Button className="navigation__save-button">Save</Button>
+                <Button className={styles.cancelButton}>Cancel</Button>
+                <Button className={styles.saveButton}>Save</Button>
             </NavigationBar>
-            <TextField label="Name of object"/>
+            <TextField className={classNames(styles.name,styles.element)} label="Name of object"/>
+            <div className={styles.container}>
+                <div className={styles.column}>
+                    <div className={styles.image}/>
+                </div>
+                <div className={styles.column}>
+                    <TextField className={styles.element} label="Package weight"/>
+                    <TextField className={styles.element} label="Category"/>
+                    <TextField className={styles.element} label="Price per package (€)"/>
+                </div>
+            </div>
         </div>)
     }
 }
 
 export default CreateOfferPage;
+

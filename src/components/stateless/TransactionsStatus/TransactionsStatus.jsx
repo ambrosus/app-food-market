d6f9ebccaf6f354013300a2d1f0e0a8b1c492863
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-07-25 12:06:53 +0200

    pooling for transaction status

--- src/components/stateless/TransactionsStatus/TransactionsStatus.jsx
+++ src/components/stateless/TransactionsStatus/TransactionsStatus.jsx
@@ -7,21 +7,21 @@ export default class TransactionsStatus extends Component {
 
     static propTypes = {
         pending: PropTypes.array.isRequired,
-        successful: PropTypes.array.isRequired,
+        success: PropTypes.array.isRequired,
         failed: PropTypes.array.isRequired,
         onClick: PropTypes.func.isRequired
     };
 
     static defaultProps = {
         pending: [],
-        successful: [],
+        success: [],
         failed: []
     };
 
     render() {
         return ( < div onClick = { () => this.props.onClick() }>
              { this.props.pending.length }
-             { this.props.successful.length }
+             { this.props.success.length }
              { this.props.failed.length }
             </div>);
     }

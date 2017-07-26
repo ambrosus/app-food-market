8ade9da1bb13cacf40201888c57c2b37115e3169
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-07-26 14:36:49 +0200

    welcome page cont.

---
+++ src/components/stateful/Pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import NavigationBar from "../../../stateless/NavigationBar/NavigationBar.jsx";
+import { Link } from 'react-router-dom';
+import Button from "../../../stateless/Button/Button.jsx";
+import InputField from "../../../stateless/InputField/InputField.jsx";
+
+
+class WelcomePage extends Component {
+    
+    render() {
+        return (
+        <div>
+            <NavigationBar title="Welcome">        
+                <Link className="navigation__link" to="/market">
+                	<Button className='navigation__create-offer-button'>
+                    	<span className="icon-basket-loaded button-icon-default"/>Create new market
+                    </Button>
+                </Link>
+            </NavigationBar>        
+    		
+    		<h1> Go to exisiting market: </h1>
+
+    		<InputField label="Market address" />
+    		<Link className="context-menu__link" to="/market/">Go</Link>		
+        </div>)
+    }
+}
+
+export default WelcomePage;

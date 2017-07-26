4826b25e0b60f4edc1aaeaf6639920065ca487c8
Marek Kirejczyk <marekkirejczyk@gmail.com>
2017-07-26 14:36:11 +0200

    welcome page

--- src/components/RootComponent.jsx
+++ src/components/RootComponent.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import React, {Component} from "react";
 import HeaderContainer from "./stateful/HeaderContainer/HeaderContainer.jsx";
 import PageContainer from "./stateless/PageContainer/PageContainer.jsx";
+import WelcomePage from "./stateful/Pages/WelcomePage/WelcomePage.jsx";
 import MarketPage from "./stateful/Pages/MarketPage/MarketPage.jsx";
 import OrdersPage from "./stateful/Pages/OrdersPage/OrdersPage.jsx";
 import ProfilePage from "./stateful/Pages/ProfilePage/ProfilePage.jsx";
@@ -17,7 +18,9 @@ export default class RootComponent extends Component {
                 <Router>
                     <PageContainer>
                         <HeaderContainer/>
-                        <Route exact path="/" component={MarketPage}/>
+                        <Route exact path="/" component={WelcomePage}/>
+                        <Route exact path="/market" component={MarketPage}/>
+                        <Route exact path="/market/:id" component={MarketPage}/>
                         <Route exact path="/orders" component={OrdersPage}/>
                         <Route exact path="/profile" component={ProfilePage}/>
                         <Route exact path="/create-offer" component={CreateOfferPage}/>

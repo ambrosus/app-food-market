470896f46517a23af75218235b635fc39d2a8044
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-07-31 12:04:16 +0200

    ConfirmBuyModal styling

--- src/components/stateless/ConfirmBuyModal/ConfirmBuyModal.jsx
+++ src/components/stateless/ConfirmBuyModal/ConfirmBuyModal.jsx
@@ -3,6 +3,7 @@ import cx from "classnames";
 import styles from "./ConfirmBuyModal.scss";
 import Label from "../Label/Label";
 import Button from "../Button/Button";
+import AttributeValueFieldContainer from "../AttributeValueFieldContainer/AttributeValueFieldContainer";
 
 export default class ConfirmBuyModal extends Component {
     render() {
@@ -10,7 +11,20 @@ export default class ConfirmBuyModal extends Component {
             <div className={styles.inner}>
                 <div className={styles.upper}>
                     <Label className={styles.title} text="Confirm buy"/>
-                    <span>We put 140 EUR tokens into ESCROW</span>
+                    <div className={styles.description}>We put 140 EUR tokens into ESCROW</div>
+                    <div className={styles.table}>
+                        <AttributeValueFieldContainer
+                            options={[
+                                {field: 'Price', value: '35 euro / kg'},
+                                {field: 'Price per package', value: '140 euro'},
+                                {field: 'Per package', value: '4 kg'},
+                                ]}/>
+                        <AttributeValueFieldContainer
+                            options={[
+                                {field: 'TOTAL WEIGHT', value: '4 kg'},
+                                {field: 'TOTAL PRICE', value: '140 euro'},
+                        ]}/>
+                    </div>
                 </div>
                 <div className={styles.lower}>
                     <div><input id="checkBox" type="checkbox"/> I want to insure my delivery for 2% of value of delivery

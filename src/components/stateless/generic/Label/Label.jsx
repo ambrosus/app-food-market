f6daa45c8432b79368a9de01f0686e8d60cdb234
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-01 16:22:39 +0200

    Welcome page styling

--- src/components/stateless/generic/Label/Label.jsx
+++ src/components/stateless/generic/Label/Label.jsx
@@ -1,7 +1,6 @@
-import React, {Component} from "react";
+import React from "react";
 import cx from 'classnames';
 import styles from "./Label.scss";
 
 let Label  = ({ text, className }) => (<span className={cx(styles.label, className)}>{ text }</span>);
-
 export default Label;
--- src/components/stateless/specific/pages/WelcomePage/WelcomePage.jsx
+++ src/components/stateless/specific/pages/WelcomePage/WelcomePage.jsx
@@ -1,34 +1,30 @@
 import React, {Component} from "react";
 import NavigationBar from "../../navigation/NavigationBar/NavigationBar";
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Button from "../../../generic/Button/Button";
-import InputField from "../../../generic/InputField/InputField";
 import TextField from "../../../generic/TextField/TextField";
-import { createBrowserHistory } from "history";
-
+import {createBrowserHistory} from "history";
+import Label from "../../../generic/Label/Label";
 
 class WelcomePage extends Component {
-    
+
     render() {
         window.his1 = createBrowserHistory();
         window.his2 = this.props.history;
         return (
-        <div>
-            <NavigationBar title="Welcome">        
-                <Link className="navigation__link" to="/create-market">
-                	<Button className='navigation__create-offer-button'>
-                    	Create new market
-                    </Button>
-                </Link>
-            </NavigationBar>        
-    		
-    		<h1> Enter address of a market: </h1>
-
-    		<TextField label="Market address" inputRef={el => this.addressField = el}/> 
-            <br/>
-            <br/>
-            <Button className='navigation__create-offer-button' onClick={ (e) => this.props.goOnClick(this.addressField.value) }> Go </Button>
-        </div>)
+            <div>
+                <NavigationBar title="Welcome">
+                    <Link className="navigation__link" to="/create-market">
+                        <Button className='navigation__create-offer-button'>
+                            Create new market
+                        </Button>
+                    </Link>
+                </NavigationBar>
+                <Label text="Enter address of a market:"/>
+                <TextField label="Market address" inputRef={el => this.addressField = el}/>
+                <Button className='navigation__create-offer-button'
+                        onClick={(e) => this.props.goOnClick(this.addressField.value)}> Go </Button>
+            </div>)
     }
 }
 

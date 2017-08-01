a8c762a27f027467ff9a695edc2bd49f130a483d
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-01 12:48:03 +0200

    Fixing locations

--- src/components/stateless/specific/containers/ModalContainer/ModalContainer.jsx
+++ src/components/stateless/specific/containers/ModalContainer/ModalContainer.jsx
@@ -1,13 +1,10 @@
 import React, {Component} from "react";
 import {connect} from 'react-redux';
 import PropTypes from "prop-types";
-import BalanceTooLowModal from "../BalanceTooLowModal/BalanceTooLowModal";
-import ErrorModal from "../ErrorModal/ErrorModal";
-import TransactionProgressModal from "../TransactionProgressModal/TransactionProgressModal";
-import ConfirmBuyModal from "../ConfirmBuyModal/ConfirmBuyModal";
-import BalanceTooLowModal from "../../modals/BalanceTooLowModal/BalanceTooLowModal";
 import TransactionProgressModal from "../../modals/TransactionProgressModal/TransactionProgressModal";
 import ConfirmBuyModal from "../../modals/ConfirmBuyModal/ConfirmBuyModal";
+import ErrorModal from "../../ErrorModal/ErrorModal";
+import BalanceTooLowModal from "../../modals/BalanceTooLowModal/BalanceTooLowModal";
 
 const mapStateToProps = (state) => {
     return {
@@ -29,13 +26,13 @@ class ModalContainer extends Component {
     };
 
     render() {
-        if (this.props.name == "ConfirmBuyModal")
+        if (this.props.name === "ConfirmBuyModal")
             return (<ConfirmBuyModal/>);
-        else if (this.props.name == "ErrorModal")
+        else if (this.props.name === "ErrorModal")
             return (<ErrorModal/>);
-    	else if (this.props.name == "BalanceTooLowModal")
+    	else if (this.props.name === "BalanceTooLowModal")
     		return (<BalanceTooLowModal/>);
-    	else if (this.props.name == "TransactionProgressModal")
+    	else if (this.props.name === "TransactionProgressModal")
     		return (<TransactionProgressModal/>);
     	else 
     		return null;

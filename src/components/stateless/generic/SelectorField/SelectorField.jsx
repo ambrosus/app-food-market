8509b43788c73f2c5809fe3062ed25ede80b4ac0
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-02 12:03:08 +0200

    Remove errors message

--- src/components/stateless/generic/SelectorField/SelectorField.jsx
+++ src/components/stateless/generic/SelectorField/SelectorField.jsx
@@ -4,11 +4,15 @@ import styles from "./SelectorField.scss";
 
 export default class SelectorField extends Component {
 
+    static defaultProps = {
+       onChange: () => { console.log('onChange is not defined')}
+    };
+
     render() {
         return (<div>
             <select className={cx(styles.select, this.props.className)} 
                     ref={this.props.inputRef}
-                    onChange={e => this.props.onChange(e.target.value)}
+                    onChange={(e) => this.props.onChange(e.target.value)}
                     value={this.props.value}>
                 { this.props.options.map((option, index) => <option key={index}>{option.value}</option>)}
             </select>

31d780bc5e2e781abfa82ffbffc94ae92e687ab6
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-02 17:55:19 +0200

    Create requirements, adding requirements

--- src/components/stateless/specific/pages/CreateRequirements/CreateRequirements.jsx
+++ src/components/stateless/specific/pages/CreateRequirements/CreateRequirements.jsx
@@ -8,33 +8,55 @@ import SelectorField from "../../../generic/SelectorField/SelectorField";
 
 class CreateRequirements extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            requirements: this.props.requirements
+        }
+    }
+
+    static defaultProps = {
+        requirements: []
+    };
+
+    addRow() {
+        let clone = this.state.requirements.slice();
+        clone.push({});
+        this.setState({
+            requirements: clone
+        })
+    }
+
+    onCancel() {
+        this.props.history.goBack();
+    }
+
+    onSave() {
+        console.log('Saving');
+    }
+
     render() {
+        { console.log(this.state.requirements) }
         return (<div>
             <NavigationBar title="Create requirements">
                 <Button className={styles.cancelButton}
-                        onClick={this.props.history.goBack}>Cancel</Button>
+                        onClick={this.onCancel.bind(this)}>Cancel</Button>
                 <Button className={styles.saveButton}
-                        onClick={() => this.onSaveClick()}>Save</Button>
+                        onClick={this.onSave.bind(this)}>Save</Button>
             </NavigationBar>
             <Label className={styles.label} text="Quality standard name:"/>
             <TextField className={styles.qualityStandard}/>
             <Label text="Attributes:" className={styles.section}/>
             <div className={styles.list}>
-                <div className={styles.row}>
+                {this.state.requirements.map((element, index) => (<div key={index} className={styles.row}>
                     <TextField/>
                     <SelectorField options={[{value: 'Range'}, {value: 'Boolean'}]} className={styles.selector}/>
                     <TextField className={styles.selector}/>
                     <TextField className={styles.selector}/>
                     <TextField className={styles.selector}/>
-                </div>
-                <div className={styles.row}>
-                    <TextField/>
-                    <SelectorField options={[{value: 'Range'}, {value: 'Boolean'}]} className={styles.selector}/>
-                    <TextField className={styles.selector}/>
-                    <TextField className={styles.selector}/>
-                    <TextField className={styles.selector}/>
-                </div>
+                </div>))}
             </div>
+            <Button onClick={this.addRow.bind(this)} className={styles.addRequirement}>Add requirement</Button>
         </div>)
     }
 }

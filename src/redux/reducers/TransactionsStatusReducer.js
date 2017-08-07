f3270fa2c708a1a2aa0a4676b679143f7c82a9b7
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-07 14:16:40 +0200

    Updating notifications work

--- src/redux/reducers/TransactionsStatusReducer.js
+++ src/redux/reducers/TransactionsStatusReducer.js
@@ -2,6 +2,13 @@ const transactionsStatus = (state = {list: []}, action) => {
     switch (action.type) {
         case 'STATUS_ADD_SUCCESS_TRANSACTION':
         case 'STATUS_ADD_FAILED_TRANSACTION':
+            let filtered = state.list.filter((transaction) => {
+                return transaction.address !== action.data.address
+            });
+
+            return {
+                list: [...filtered, action.data]
+            };
         case 'STATUS_ADD_PENDING_TRANSACTION':
             return {
                 list: [...state.list, action.data]

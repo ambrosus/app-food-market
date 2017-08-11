9180264f1abe001a6891d3900177dbe27a678059
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-11 11:55:39 +0200

    WelcomePage styling

--- src/components/stateless/specific/pages/WelcomePage/WelcomePage.jsx
+++ src/components/stateless/specific/pages/WelcomePage/WelcomePage.jsx
@@ -10,12 +10,13 @@ class WelcomePage extends Component {
   render() {
     return (
       <div className={styles.page}>
+        <img className={styles.logo} src="./static/images/ambrosus.png"/>
         <div className={styles.container}>
-          <Label className={styles.header} text='Welcome' />
+          <Label className={styles.header} text='Welcome'/>
           <div>
-            <Label className={styles.label} text='Go to existing market:' />
+            <Label className={styles.label} text='Go to existing market:'/>
             <div className={styles.row}>
-              <TextField placeholder={'contact address'} className={styles.field} />
+              <TextField placeholder={'contact address'} className={styles.field}/>
               <Button className={styles.button}><Link className={styles.link} to="/">Go</Link></Button>
             </div>
           </div>
@@ -23,6 +24,7 @@ class WelcomePage extends Component {
           <Button className={styles.newAccount}>
             <Link className={styles.link} to="/create-market">Create new market</Link></Button>
         </div>
+        <img className={styles.smallLogo} src="./static/images/ambrosus-small.png" />
       </div>);
   }
 }

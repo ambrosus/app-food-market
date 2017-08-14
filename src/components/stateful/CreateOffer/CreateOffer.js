cc67dee26803fd483460c2fa544b44aa24a5be64
ivan <ivanruch@gmail.com>
2017-08-14 16:34:49 +0300

    show attributes in product create offer page

--- src/components/stateful/CreateOffer/CreateOffer.js
+++ src/components/stateful/CreateOffer/CreateOffer.js
@@ -1,17 +1,24 @@
 import { connect } from 'react-redux';
 import CreateOfferPage from '../../stateless/specific/pages/CreateOfferPage/CreateOfferPage';
 import { createOffer } from '../../../redux/actions/OfferAction.js';
+import { fetchAttributesFromQualityName } from '../../../redux/actions/AttributesAction';
 
 const mapStateToProps = (state) => ({
   address: state.market.address,
   categories: state.categories,
-  qualities: state.market.qualities,
+  qualities: ['None', ...state.market.qualities],
+  attributes: state.attributes,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onAdd: (offer, image, marketAddress) => {
     dispatch(createOffer(offer, image, marketAddress, ownProps.history));
   },
+
+  fetchAttributes: (requirementsName, marketAddress) => {
+    dispatch(fetchAttributesFromQualityName(requirementsName, marketAddress));
+  },
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateOfferPage);

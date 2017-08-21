c73b1c222fc94e4123ac6281795e949839278c8f
ivan <ivanruch@gmail.com>
2017-08-21 13:41:32 +0200

    Merging

--- src/redux/reducers/TokenReducer.js
+++ src/redux/reducers/TokenReducer.js
@@ -1,7 +1,5 @@
 const token = (state = { token: {}, balance: 0 }, action) => {
   switch (action.type) {
-    case 'FETCH_TOKEN_SUCCESS':
-      return { ...state, token: action.token };
     case 'UPDATE_BALANCE_SUCCESS':
       return { ...state, balance: action.balance };
     default:

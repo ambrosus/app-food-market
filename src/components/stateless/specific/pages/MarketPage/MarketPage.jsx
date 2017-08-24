aae3af64707547002e37e232b81fc10a66e0bb66
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-08-24 12:33:31 +0200

    Selector default behaviour

--- src/components/stateless/specific/pages/MarketPage/MarketPage.jsx
+++ src/components/stateless/specific/pages/MarketPage/MarketPage.jsx
@@ -73,18 +73,21 @@ class MarketPage extends Component {
   }
 
   render() {
-    var filteredOffers = this.getFilteredRequirement(this.getFilteredCategories(this.props.offers));
+    let filteredOffers = this.getFilteredRequirement(this.getFilteredCategories(this.props.offers));
     return (
       <div>
         <NavigationBar title='Market'>
           <Label text='Quality:'/>
           <SelectorField className={styles.selector} options={this.getRequirements()}
-                         label='selectedRequirement' onChange={this.onChange.bind(this)}
+                         label='selectedRequirement'
+                         placeholder="Select quality"
+                         onChange={this.onChange.bind(this)}
                          value={this.state.selectedRequirement}/>
           <Label text='Categories:'/>
           <SelectorField className={styles.selector} options={this.getCategories()}
                          label='selectedCategory'
                          onChange={this.onChange.bind(this)}
+                         placeholder="Select category"
                          value={this.state.selectedCategory}/>
           <Link className='navigation__link' to='/create-offer'><Button
             className='navigation__create-offer-button'>

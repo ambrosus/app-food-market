899a26b41e66fde393b71f2eb5f8bec32158072f
ivan <ivanruch@gmail.com>
2017-08-31 16:42:39 +0200

    Optimize selecting breadcrumbs

--- src/components/stateless/specific/navigation/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/stateless/specific/navigation/Breadcrumbs/Breadcrumbs.jsx
@@ -7,17 +7,25 @@ import styles from './Breadcrumbs.scss';
 export default class Breadcrumbs extends Component {
 
   render() {
-    console.log(this.props.breadcrumbs);
     return (<div className={cx(styles.breadcrumbs, this.props.className)}>
       {
         _(this.props.breadcrumbs)
           .dropRight()
-          .map(page => (<span className={styles.breadcrumbs} key={page.to}>
-              <Link className={styles.link} to={page.to}>{page.name}</Link> &gt;&nbsp;
-            </span>))
-          .concat(<span className={styles.breadcrumbs} key=''>{_.last(this.props.breadcrumbs).name}</span>)
+          .map(page => Breadcrumbs.renderActive(page))
+          .concat(this.renderInactive())
           .value()
       }
     </div>);
+  }
+
+  renderInactive() {
+    return this.props.breadcrumbs.length > 0 ? (
+      <span className={styles.breadcrumbs} key=''>{_.last(this.props.breadcrumbs).name}</span>) : [];
+  }
+
+  static renderActive(page) {
+    return (<span className={styles.breadcrumbs} key={page.to}>
+              <Link className={styles.link} to={page.to}>{page.name}</Link> &gt;&nbsp;
+            </span>);
   }
 };

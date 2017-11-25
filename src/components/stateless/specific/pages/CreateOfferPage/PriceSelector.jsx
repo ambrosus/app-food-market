68f654475e0784a8e119172d11bdd80fd1b053d1
Joseph C Wang <joequant@gmail.com>
2017-11-26 03:02:19 +0800

    more refactoring

--- src/components/stateless/specific/pages/CreateOfferPage/PriceSelector.jsx
+++ src/components/stateless/specific/pages/CreateOfferPage/PriceSelector.jsx
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import Label from '../../../generic/Label/Label.jsx';
 import InputField from '../../../generic/InputField/InputField';
 
 export default class QuantitySelector extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { value: props.value };
   }
-
-  static propTypes = {
-    className: PropTypes.string,
-    placeholder: PropTypes.string,
-    onChange: PropTypes.func,
-  };
 
   render() {
   return (<InputField text='Unit Price (€)'
                             className={this.props.className}
                             errors={this.props.errors}
                             onChange={this.props.onChange}
-                            label='packageWeight'/>);
+                            label='unitPrice'/>);
   }
 };
--- src/components/stateless/specific/pages/CreateOfferPage/QuantitySelector.jsx
+++ src/components/stateless/specific/pages/CreateOfferPage/QuantitySelector.jsx
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import Label from '../../../generic/Label/Label';
 import InputField from '../../../generic/InputField/InputField';
-import SelectorField from '../../../generic/SelectorField/SelectorField';
 
 export default class QuantitySelector extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { value: props.value };
   }
-
-  static propTypes = {
-    className: PropTypes.string,
-    placeholder: PropTypes.string,
-    onChange: PropTypes.func,
-  };
-
   render() {
   return (
                  <InputField text='Package weight (kg)'

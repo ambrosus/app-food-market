616824ee84e772ca2be0f28e4123fcfe923e6364
Joseph C Wang <joequant@gmail.com>
2017-11-25 18:34:08 +0800

    add TextArea component

---
+++ src/components/stateless/generic/TextArea/TextArea.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './TextArea.scss';
+import ErrorList from '../ErrorList/ErrorList';
+
+export default class TextArea extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: props.value };
+  }
+
+  static propTypes = {
+    className: PropTypes.string,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    errors: PropTypes.arrayOf(PropTypes.string),
+  };
+
+  static defaultProps = {
+    errors: [],
+  };
+
+  render() {
+    return (<span className={this.props.className}>
+      <textarea ref="textarea"
+             className={classNames(styles.input, this.props.className)}
+             onChange={this.onChange.bind(this)}
+             placeholder={this.props.placeholder}
+             value={this.state.value}/>
+      <ErrorList errors={this.props.errors}/>
+    </span>);
+  }
+
+  onChange() {
+    let state = {
+      value: this.refs.input.value,
+    };
+    this.setState(state, this.props.onChange.bind(this, this.props.label, state));
+  }
+};

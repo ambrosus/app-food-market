e5468b62c36fdea8e7a7646d7f323b9b92b009a7
vetalxpro <vetalpro.exe@gmail.com>
2018-01-17 15:17:07 +0200

    Changed TRADES_LIST data.

--- src/redux/actions/AgreementsAction.js
+++ src/redux/actions/AgreementsAction.js
@@ -30,21 +30,21 @@ const NAMES_LENGTH = NAMES_LIST.length;
 
 for (let i = 0; i < 45; i++) {
   TRADES_LIST.push({
-    id: `${Math.random()}`,
+    id: i,
     name: NAMES_LIST[Math.floor(Math.random() * (NAMES_LENGTH - 1))],
     origin: '',
     category: 'Coffee',
     seller: `0x0056bd78b3c0d85e0ceb4a7634368845a21d${Math.random()}ea38`,
     imageHash: IMAGES_LIST[Math.floor(Math.random() * (IMAGES_LENGTH - 1))],
     status: 'finished',
-    packageWeight: 2,
-    pricePerPackage: 200,
+    packageWeight: Math.round(Math.random() * (200 - 60) + 60),
+    pricePerPackage: parseFloat((Math.random() * (200 - 60) + 60), 2),
     measurementsAddress: '0xe3fa9f8dc84c3f1629d94f6b969b197920cea712',
     requirementsAddress: '0xe18bd8f3674b3903eeb5c0331319e12f83fb519b',
     validatorAddress: '0x0000000000000000000000000000000000000000',
     address: `0x0000000000000000000000${Math.random()}000000000000000000`,
     pricePerUnit: 100,
-    quantity: Math.random() * (20 - 2) + 20,
+    quantity: Math.round(Math.random() * (20 - 2) + 20),
     quality: 'High',
   });
 };
